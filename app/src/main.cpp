83c5e0354f0d0228257477d883993467da6b341f
lcaxeixa <luiza.caxeixa@icomp.ufam.edu.br>
2025-10-16 14:03:48 -0400

    mexi no docker-compose e dockerfile

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,21 @@
-// src/main.cpp
 #include <iostream>
+#include <fstream>
+#include <cstdlib> // para getenv
+#include <string>
 
 int main() {
-    std::cout << "Build funcionando!" << std::endl;
+    const char* logLevel = std::getenv("LOG_LEVEL");
+    const char* dataDir = std::getenv("DATA_DIR");
+
+    std::string logPath = std::string(dataDir ? dataDir : ".") + "/log.txt";
+    std::ofstream logFile(logPath, std::ios::app);
+
+    if (logFile.is_open()) {
+        logFile << "[INFO] Programa iniciado com LOG_LEVEL=" 
+                << (logLevel ? logLevel : "NULO") << std::endl;
+        logFile.close();
+    }
+
+    std::cout << "Log gravado em " << logPath << std::endl;
     return 0;
 }
